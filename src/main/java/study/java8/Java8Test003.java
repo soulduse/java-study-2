0b118e0aaa994a6cf4f8ff33e5a4c524eb1aa89e
soulduse <soulduse@naver.com>
2016-12-24 15:38:26 -0600

    update some Algorithm

--- src/main/java/study/java8/Java8Test003.java
+++ src/main/java/study/java8/Java8Test003.java
@@ -10,9 +10,28 @@ public class Java8Test003 {
         GreetingService greetingService = messsage ->
                 System.out.println(salutation+messsage);
         greetingService.sayMessage("Dave");
+        isddd("ABCDE");
     }
 
     interface GreetingService{
         void sayMessage(String messsage);
+    }
+
+    static boolean isddd(String word){
+        try{
+            String reverseStr[] = word.split("");
+            String tempStr = "";
+            for(int i=reverseStr.length-1; i>=0; i--){
+                tempStr += reverseStr[i];
+            }
+
+            if(word.equalsIgnoreCase(tempStr))
+                return true;
+            else
+                return false;
+
+        }catch (Exception e){
+            throw new UnsupportedOperationException("okok");
+        }
     }
 }
